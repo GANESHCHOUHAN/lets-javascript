28cd509936ef8bacad3f327e89772eca089e7406
GANESHCHOUHAN <129497236+GANESHCHOUHAN@users.noreply.github.com>
2024-05-18 09:12:25 +0000

    basics of javascript

---
+++ basicss.js
@@ -0,0 +1,8 @@
+const accountId = 112007
+let accountEmail = "ganesh@gmail.com"
+var acoountPassword = "123456"
+accountCity = "Indore"
+var acoountPassword = "748956"
+accountCity = "Indore,bhopal"
+accountEmail = "chouhan@gmail.com"
+console.table([accountEmail,accountId,accountCity,acoountPassword])
