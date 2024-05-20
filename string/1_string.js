ae490352978c08a5b4334e20818ca2cdb63c921f
GANESHCHOUHAN <129497236+GANESHCHOUHAN@users.noreply.github.com>
2024-05-20 16:34:37 +0000

    string concept

---
+++ string/1_string.js
@@ -0,0 +1,15 @@
+const myName ="ganesh"
+const surName = "chouhan"
+
+console.log(`my name is ${myName} and Surname is ${surName} `);
+console.log(myName + " " +  surName);
+
+console.log(myName.charAt(2));
+console.log(surName.length)
+
+const newstring = new String (`hello-world`)
+console.log(newstring.replace("-",`my`));
+
+// console.log(surName.indexOf('a'));
+console.log(surName.toUpperCase());
+
